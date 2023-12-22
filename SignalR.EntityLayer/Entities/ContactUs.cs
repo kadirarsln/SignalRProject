aacd0e0480e3f4af8e34baa9816375c6fdf5b8c2
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-12-22 16:50:13 +0300

    Entities added and NugetPackage Added.

---
+++ SignalR.EntityLayer/Entities/ContactUs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class ContactUs
+    {
+        public int ContactID { get; set; }
+        public string? Location { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? FooterDescription { get; set; }
+    }
+}
---
+++ SignalR.EntityLayer/Entities/SocialMedia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaID { get; set; }
+        public string? SocialMediaTitle { get; }
+        public string? SocialMediaIcon { get; }
+        public string? SocialMediaUrl { get; }
+    }
+}
