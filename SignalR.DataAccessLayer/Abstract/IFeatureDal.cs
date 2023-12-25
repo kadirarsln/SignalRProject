f7bdc0a3e6addd103b1efe4d602c85edae44ef79
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-12-25 16:47:00 +0300

    Interfaces added.

---
+++ SignalR.DataAccessLayer/Abstract/IFeatureDal.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IFeatureDal : IGenericDal<Feature>
+    {
+        // Feature entity'sine özel işlemler buraya eklenebilir
+        // Örneğin: Feature'e özel sorgular, filtrelemeler, vb.
+    }
+}
---
+++ SignalR.DataAccessLayer/Abstract/ITestimonialDal.cs
@@ -0,0 +1,14 @@
+﻿using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface ITestimonialDal : IGenericDal<Testimonial>
+    {
+        // Testimonial entity'sine özel işlemler buraya eklenebilir
+    }
+}
